75b947824ec85d0506cb21ccd88737bf2414bac1
CoderTypist <cjbargraser@gmail.com>
2019-07-05 21:49:59 -0500

    Updated README.md

---
+++ generateComparisons/README.md
@@ -0,0 +1,23 @@
+# generateComparisons
+
+
+DESCRIPTION
+
+
+Bash script that generates a header file (Comparisons.h) to be used by LinkedList.h
+
+
+WHAT IS Comparisons.h?
+Each Linked List must be given a function to compare values. That means that the user must write a function to compare the data type being stored in the list. Comparisons.h contians functions that will compare most of the built in types in C
+
+
+USAGE (Generates Comparisons.h)
+
+
+You must have generateComparisons.sh, typeList.txt, comparisonTemplate.txt, and compareString.txt in the same folder
+
+
+chmod u+x generateComparisons.sh
+
+
+./generateComparisons.sh < typeList.txt
---
+++ generateComparisons/generateComparisons.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# author Christian Bargraser
+
+echo "// author Christian Bargraser" > ./Comparisons.h
+echo "" >> ./Comparisons.h
+echo "#ifndef _Comparisons_h_" >> ./Comparisons.h
+echo "#define _Comparisons_h_" >> ./Comparisons.h
+echo "" >> ./Comparisons.h
+echo "#define EQAUL 0" >> ./Comparisons.h
+echo "#define LESS_THAN 1" >> ./Comparisons.h
+echo "#define GREATER_THAN 2" >> ./Comparisons.h
+
+echo "" > ./comparisonPrototypes.txt
+echo "" > ./comparisonFunctions.txt
+
+while read unspaced && read spacedOut; do
+    
+    # reads the first line from the template
+    read firstLine < ./comparisonTemplate.txt
+    firstLine=$(echo "$firstLine" | sed "s/COMPACT/$unspaced/g")
+    prototype="${firstLine};"
+    echo "$prototype" >> ./comparisonPrototypes.txt
+
+    header="${firstLine}{"
+    echo "$header" >> ./comparisonFunctions.txt
+    
+    # reads the entire template
+    isFirstLine=0
+    while IFS= read line; do
+
+        if [ 0 -eq "$isFirstLine" ]; then
+            isFirstLine=1
+            continue
+        fi
+
+        line=$(echo "$line" | sed "s/TYPE/$spacedOut/g")
+        line=$(echo "$line" | sed "s/COMPACT/$unspaced/g")
+        echo "$line" >> ./comparisonFunctions.txt
+
+    done < ./comparisonTemplate.txt
+    echo "" >> ./comparisonFunctions.txt
+
+done
+
+echo "int compareString(void *firstValue, void *secondValue);" >> ./comparisonPrototypes.txt
+cat ./compareString.txt >> ./comparisonFunctions.txt
+
+cat ./comparisonPrototypes.txt >> ./Comparisons.h
+cat ./comparisonFunctions.txt >> ./Comparisons.h
+
+echo "#endif" >> ./Comparisons.h
+ 
+
+
